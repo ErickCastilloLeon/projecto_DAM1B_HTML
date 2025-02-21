7497a7d99ac38785b69393dee486521a96325389
Erick <ecatillleo@educacion.navarra.es>
2025-02-21 17:48:31 +0100

    Apartado content
    Se desarrolló los demás apartados

---
+++ js/data_base/juegos/gamesH_content.js
@@ -0,0 +1,41 @@
+const JuegosH = [
+    {
+        TituloEspañol: "La leyenda de la reina opala II",
+        TituloOriginal: "",
+        TituloRespaldo: "",
+        Imagen: "img/content/imagenJuego.png",
+        DireccionImagen: "https://chatgpt.com/",
+        Descripcion1: "Este es un juego de aventura",
+        Descripcion2: "Ambientado en la cultura medieval",
+        Descripcion3: "Posee multiples finales",
+        Autor: "Autor 1",
+        Motor: "Unity",
+        Contraseña: "1234",
+        Tags: ["Aventura", "Acción"],
+        EnlaceDescarga1: "https://www.juego1.com/descarga",
+        EnlaceDescarga2: "https://www.juego1.com/descarga",
+        EnlaceDescarga3: "https://www.juego1.com/descarga"
+    },
+    {
+        TituloEspañol: "La leyenda de la reina opala I",
+        TituloOriginal: "",
+        TituloRespaldo: "",
+        Imagen: "img/imagenJuego.png",
+        DireccionImagen: "https://chatgpt.com/",
+        Descripcion1: "Este es un juego de aventura",
+        Descripcion2: "Ambientado en la cultura medieval",
+        Descripcion3: "Posee multiples finales",
+        Autor: "Autor 1",
+        Motor: "Unity",
+        Contraseña: "1234",
+        Tags: ["Aventura", "Acción"],
+        EnlaceDescarga1: "https://www.juego1.com/descarga",
+        EnlaceDescarga2: "https://www.juego1.com/descarga",
+        EnlaceDescarga3: "https://www.juego1.com/descarga"
+    }
+];
+
+/*
+\n
+*/
+
